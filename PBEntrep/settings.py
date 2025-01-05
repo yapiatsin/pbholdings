2c3c8299bf96abc7102376558fa9a64207e855a9
treff09 <125082554+treff09@users.noreply.github.com>
2025-01-05 09:22:46 +0000

    Update settings.py
    Signed-off-by: treff09 <125082554+treff09@users.noreply.github.com>

--- PBEntrep/settings.py
+++ PBEntrep/settings.py
@@ -10,7 +10,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = config('SECRET_KEY')
 # DEBUG = True
 
-ALLOWED_HOSTS = ['pbholdingsite.com', 'www.pbholdingsite.com']
+ALLOWED_HOSTS = ['pbholdingsite.com', 'www.pbholdingsite.com','45.92.109.86']
 CSRF_TRUSTED_ORIGINS = [
     'https://pbholdingsite.com',
 ]
