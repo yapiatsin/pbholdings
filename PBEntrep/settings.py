285729ff2a1452ec835ae75801ff3486c79e7d67
unknown <yapiatsin0@gmail.com>
2026-01-17 00:54:34 +0000

    Update all and settings P&B 17/01/2026

--- PBEntrep/settings.py
+++ PBEntrep/settings.py
@@ -12,13 +12,13 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = config('SECRET_KEY')
 # DEBUG = True--  django-insecure-of03a0_f)5yamk9g&p9p2f^a*l8!6t#+r_c4oq42+sb&#y5znt
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['localhost','127.0.0.1','192.168.100.157']
 
-# ALLOWED_HOSTS = ['pbholdingsite.com','www.pbholdingsite.com','45.92.109.86']
-# CSRF_TRUSTED_ORIGINS = [
-#     'https://pbholdingsite.com',
-# ]
+ALLOWED_HOSTS = ['pbholdingsite.com','www.pbholdingsite.com','45.92.109.86']
+CSRF_TRUSTED_ORIGINS = [
+    'https://pbholdingsite.com',
+]
 
 handler403 = 'PB_Entreprise.views.permission_denied_view'
 handler404 = 'PB_Entreprise.views.custom_404_view'
@@ -74,18 +74,18 @@ WSGI_APPLICATION = 'PBEntrep.wsgi.application'
 DEBUG = False
 # DEBUG = config('DEBUG') 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': config('DB_NAME'),
-    #     'USER':config('DB_USER'),
-    #     'PASSWORD': config('DB_PASSWORD'),
-    #     'HOST':config('DB_HOST'),
-    #     'PORT':config('DB_PORT'),
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER':config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST':config('DB_HOST'),
+        'PORT':config('DB_PORT'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
