5c861ba786c0bed8e7b23ab3de36a124aa12d887
unknown <yapiatsin0@gmail.com>
2026-06-11 02:47:27 +0000

    build: ajoute whitenoise + deps PDF, sert le static en prod

--- PBEntrep/settings.py
+++ PBEntrep/settings.py
@@ -141,8 +141,8 @@ USE_TZ = True
 
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
