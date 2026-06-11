7aacef117f6355d23c0914fb3be73bf018959053
unknown <yapiatsin0@gmail.com>
2026-06-11 02:06:53 +0000

    modification de urls de base du projet 11-06-2026 02h06

--- PBEntrep/urls.py
+++ PBEntrep/urls.py
@@ -1,26 +1,26 @@
 from django.contrib import admin
-from django.shortcuts import render
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.static import serve
-from django.urls import re_path
-import os
 
-name = 'PBFinance'
 urlpatterns = [
     path('pb/myadmin/', admin.site.urls),
-    path('',include('PBFinance.urls')),
-    path('auth/',include('userauths.urls')),
-    path('pbentreprise/',include('PB_Entreprise.urls')),
+    path('', include('PBFinance.urls')),
+    path('auth/', include('userauths.urls')),
+    path('pbentreprise/', include('PB_Entreprise.urls')),
 ]
-# Servir les fichiers média
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# Servir les fichiers statiques même avec DEBUG=False (pour le développement local)
-if not settings.DEBUG:
-    # Servir depuis STATICFILES_DIRS (le dossier static principal)
-    static_dir = settings.STATICFILES_DIRS[0] if settings.STATICFILES_DIRS else settings.STATIC_ROOT
-    urlpatterns += [
-        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': static_dir}),
-    ]
 
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # WhiteNoise sert /static/ ; media servi ici
+    urlpatterns += [
+        re_path(
+            r'^media/(?P<path>.*)$',
+            serve,
+            {'document_root': settings.MEDIA_ROOT},
+        ),
+    ]
